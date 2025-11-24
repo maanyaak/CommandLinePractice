db355b976d5688e989d9b4b8fd2c1acddd69f494
maanyaak <maanya.kavi@gmail.com>
2025-11-23 21:58:39 -0500

    newMethod added
    I added a method to print out different formatting for numbers

--- New.java
+++ New.java
@@ -1,6 +1,10 @@
 public class New
 {
-  
+
+  public void newMethod(int num)
+  {
+    
+  }
 public static void main (String[] args)
 {
   int x=0;
@@ -10,5 +14,14 @@ public static void main (String[] args)
       x++;
     }
 }
+  public void newMethod(int num)
+    {
+        System.out.printf("Right -  %10d%n", num);
+
+        System.out.printf("Leading 0s -  %010d%n", num);
+
+        System.out.printf("Comas -  %,d%n", num);
+    }
+
 
 }
